033e9b60b206b40cfb99b4c109e18eb33c7518bc
Sean Gibat <sgibat@gmail.com>
2015-02-10 15:49:54 -0800

    added directives for the article listening and reading and integrated them into the dashboard template. gonna look into using routes later, just wanted to get this up asap.

--- app/assets/javascripts/controllers/dashboard_ctrl.js
+++ app/assets/javascripts/controllers/dashboard_ctrl.js
@@ -1,3 +1,29 @@
 app.controller('DashboardCtrl', ['$scope', 'Articles', function($scope, Articles){
   $scope.articles = Articles.query();
+  $scope.reading = null;
+  $scope.listening = null;
+  $scope.paused = false;
+
+  var voice = new SpeechSynthesisUtterance();
+
+  $scope.read = function(id){
+    $scope.reading = id;
+  };
+
+  $scope.listen = function(id){
+    $scope.paused = false;
+    $scope.listening = id;
+    $scope.reading = id;
+    voice.text = $scope.articles[id].content;
+    speechUtteranceChunker(voice);
+  };
+
+  $scope.pausePlay = function(){
+    if ($scope.paused)
+      speechSynthesis.resume();
+    else
+      speechSynthesis.pause();
+    
+    $scope.paused = !$scope.paused;
+  }
 }]);
