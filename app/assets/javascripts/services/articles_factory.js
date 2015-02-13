9d65986904957f68e3f91f8d71c21f563da2206c
Kenny Czadzeck <kennycgit@gmail.com>
2015-02-13 15:42:21 -0800

    fixing namespaceing bug in function

--- app/assets/javascripts/services/articles_factory.js
+++ app/assets/javascripts/services/articles_factory.js
@@ -23,18 +23,18 @@ app.factory('Article', ['$resource', function ($resource) {
     article.$save(function(savedArticle) {
       Article.get({id: savedArticle.id}, function(foundArticle) {
         foundArticle.isArticle = true;
-        var sessionStorageArticles = sessionStorage.getItem('articles');
-        if(sessionStorageArticles) {
-          sessionStorageArticles = JSON.parse(sessionStorageArticles);
-          sessionStorageArticles.push(foundArticle);
+        var sessionArticles = sessionStorage.getItem('articles');
+        if(sessionArticles) {
+          sessionArticles = JSON.parse(sessionArticles);
+          sessionArticles.push(foundArticle);
         } else {
           sessionStorage.setItem('articles', JSON.stringify(foundArticle));
         }
         if(callback) {
-          callback(sessionStorageArticles || foundArticle);
+          callback(sessionArticles || foundArticle);
         }
-        sessionStorageArticles = JSON.stringify(sessionStorageArticles);
-        sessionStorage.setItem('articles', sessionStorageArticles);
+        sessionArticles = JSON.stringify(sessionArticles);
+        sessionStorage.setItem('articles', sessionArticles);
       });
     });
   }
@@ -43,20 +43,16 @@ app.factory('Article', ['$resource', function ($resource) {
     article.archive = true;
     Article.update({id: article.id}, article, function(data) {
       Article.get({id: data.id}, function(foundArticle) {
-        var sessionStorageArticles = sessionStorage.getItem('articles');
-        sessionStorageArticles = JSON.parse(sessionStorageArticles);
-        var len = sessionStorageArticles.length;
-        for (var i=0; i < len; i++) {
-          if (foundArticle.id === sessionStorageArticles[i].id) {
-            sessionStorageArticles.splice(i, 1);
-            if(sessionArticles.length === 0) {
-              sessionStorage.removeItem('articles');
-            }
+        var sessionArticles = sessionStorage.getItem('articles');
+        sessionArticles = JSON.parse(sessionArticles);
+        for (var i=0; i < sessionArticles.length; i++) {
+          if (foundArticle.id === sessionArticles[i].id) {
+            sessionArticles.splice(i, 1);
             if (callback) {
-              callback(sessionStorageArticles);
+              callback(sessionArticles);
             }
-            sessionStorageArticles = JSON.stringify(sessionStorageArticles);
-            sessionStorage.setItem('articles', sessionStorageArticles);
+            sessionArticles = JSON.stringify(sessionArticles);
+            sessionStorage.setItem('articles', sessionArticles);
             break;
           }
         }
