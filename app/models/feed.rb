947bdf60633fb453d9fc718c506bfdd606a82075
Krish Rajagopalan <krish.andres@gmail.com>
2015-02-19 14:34:47 -0800

    Refactor feed model so that delete feeds button works correctly

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -10,28 +10,50 @@ class Feed < ActiveRecord::Base
     arr_feeds = []
     urls = []
     feeds.each do |feed|
-      urls.push feed.url
-    end
-    feedjira_feeds = Feedjira::Feed.fetch_and_parse urls
+      # urls.push feed.url
 
+      this_feed = Feedjira::Feed.fetch_and_parse feed.url
 
-    id_count = 0
-    feedjira_feeds.each{ |key, feed|
-      this_id = feeds[id_count].id
-      id_count += 1
+      this_feed.entries = this_feed.entries.map { |entry| 
+        entry.inject({}) { |obj, attr| 
+          obj[attr[0]] = attr[1]; 
+          obj 
+        } 
+      }
+
       arr_feeds.push({
-        title: feed.title,
-        description: feed.description,
-        url: feed.url,
-        id: this_id,
-        entries: feed.entries.map { |entry| 
-          entry.inject({}) { |obj, attr| 
-            obj[attr[0]] = attr[1]; 
-            obj 
-          } 
-        }
-      })
-    }
+        id: feed.id, 
+        title: this_feed.title, 
+        description: this_feed.description,
+        url: this_feed.url,
+        entries: this_feed.entries
+        })
+    end
+
+    # feedjira_feeds = Feedjira::Feed.fetch_and_parse urls
+
+    # puts feeds
+
+    # feedjira_feeds.each{ |key, feed|
+
+    #   this_feed = feeds.select { |f| puts f.url; f.url.include?(feed.url) }
+    #   puts feed.url
+    #   puts this_feed
+ 
+    #   arr_feeds.push({
+    #     title: feed.title,
+    #     description: feed.description,
+    #     url: feed.url,
+    #     id: this_feed.first.id,
+    #     entries: feed.entries.map { |entry| 
+    #       entry.inject({}) { |obj, attr| 
+    #         obj[attr[0]] = attr[1]; 
+    #         obj 
+    #       } 
+    #     }
+    #   })
+    # }
+
     return arr_feeds
   end
 
@@ -44,6 +66,15 @@ class Feed < ActiveRecord::Base
         obj 
       } 
     }
-    return this_feed
+
+    big_object = {
+      id: feed.id, 
+      title: this_feed.title, 
+      description: this_feed.description,
+      url: this_feed.url,
+      entries: this_feed.entries
+    }
+
+    return big_object
   end
 end
